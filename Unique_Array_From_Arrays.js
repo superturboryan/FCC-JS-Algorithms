0e25106027f54b754a775a0265ba539765bc5ce0
superturboryan <forsyth.r@gmail.com>
2020-01-05 12:50:36 -0500

    Complete unique array from arrays

---
+++ Unique_Array_From_Arrays.js
@@ -0,0 +1,23 @@
+/*
+Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+*/
+
+function uniteUnique(arr) {
+    let nums = []
+    
+    for (let i in arguments) {
+      for (let num of arguments[i]) {
+        if (!nums.includes(num)) {
+          nums.push(num)
+        }
+      }
+    }
+    return nums
+  }
+  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
